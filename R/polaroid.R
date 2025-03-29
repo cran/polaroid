fc12561f73813fd64edd09ab5cd9bbc5b0fddf6e
Jinhwan Kim <hwanistic@gmail.com>
2025-03-29 16:10:02 +0000

    version 0.2.1

--- R/polaroid.R
+++ R/polaroid.R
@@ -3,7 +3,9 @@
 #' @description run polaroid shiny application in appDir
 #' @seealso https://github.com/jhk0530/polaroid
 #'
-#' @examples if(interactive()){ polaroid() }
+#' @examples if (interactive()) {
+#'   polaroid()
+#' }
 #'
 #' @import shiny
 #' @return None
@@ -11,18 +13,26 @@
 #' @export
 #'
 
-
 polaroid <- function() {
+  required_pkgs <- c("argonDash", "argonR", "colourpicker", "hexSticker",
+                     "htmltools", "magick", "shinyjs", "showtext",
+                     "stringr", "sysfonts")
 
-  appDir <- system.file("polaroid", package = "polaroid")
-  if (appDir == "") {
+  missing_pkgs <- required_pkgs[!vapply(required_pkgs, requireNamespace, logical(1), quietly = TRUE)]
+
+  if (length(missing_pkgs) > 0) {
     stop(
-      "Could not find Directory, Try re-install",
+      paste("Please install missing packages:", paste(missing_pkgs, collapse = ", ")),
       call. = FALSE
     )
   }
 
-  runApp(
+  appDir <- system.file("polaroid", package = "polaroid")
+  if (appDir == "") {
+    stop("Could not find Directory, Try re-install", call. = FALSE)
+  }
+
+  shiny::runApp(
     appDir,
     launch.browser = TRUE,
     display.mode = "normal"
